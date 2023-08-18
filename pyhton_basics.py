f1baaf25f782c142e7dc14af7795f680a9e69f73
Marnimarns <142357299+Marnimarns@users.noreply.github.com>
2023-08-18 22:58:31 +0100

    Add files via upload
    My First introductions to python - Syntax related topics

---
+++ pyhton_basics.py
@@ -0,0 +1,40 @@
+Syntax
+Indentantion
+
+If True:
+        print ('hello world')
+        print ('Goodbye world')
+       print('Good day')
+
+# The use of indentation can help tp clean code up visually, however be careful that for all indentaion for one block is identicle.
+
+# Single Line Comment
+
+"""
+Multi line comment
+Line 1
+Line 2 
+Line 3 - Sign off with the triple 
+
+"""
+# Variables
+name = Financial Data Scienctist
+num1 = 5
+num2 = 7
+num3 = num1 + num2
+num6 = num1
+num5 = 2 * num6
+num6
+
+
+# Multiline statements - ;
+
+num1 = 7; num2 = 3
+num3 = num2 + num1
+
+
+# Continuance - /  To seperate a single line to multiple lines the use of / tells the interpreter to continue reading from the line below
+
+
+
+# Print - print() to print variables or strings ect
